c6ff63bd8513a90c04d86b06ddecc01dc96fef61
Koumei <t.koumei.0121@gmail.com>
2023-10-23 15:08:10 +0900

    変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def top
   end
+  def keisan
+    weigh = params[:weigh]
+    small = params[:small]
+    move = params[:move_time]
+    comparison = weigh.to_i - small.to_i
+    sweat = comparison / move.to_i
+    p weigh
+    p small
+    p comparison
+    p sweat
+    redirect_to home_top_path
+  end
 end
